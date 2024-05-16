702d519929370c9da3209110df6ad48a61f09c6e
Scott Cain <scott+git@scottcain.net>
2024-05-16 09:34:44 -0700

    adding the script to run gen-names and upload to aws

--- gen_names.sh
+++ gen_names.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-echo 'do nothing'
+AWSACCESS=${AWS_ACCESS_KEY}
+AWSSECRET=${AWS_SECRET_KEY}
+
+cd /usr/share/nginx/html/jbrowse
+
+bin/generate-names.pl 
+
+AWS_ACCESS_KEY_ID=$AWSACCESS AWS_SECRET_ACCESS_KEY=$AWSSECRET aws s3 cp --recursive --acl public-read data/names s3://agrjbrowse/test/sgd-names/
+
