f301a7db2fbfc0809f30d785c710174646959dea
Scott Cain <scott+git@scottcain.net>
2024-05-20 11:23:40 -0700

    should be enough to add gff processing to this container

--- gen_names.sh
+++ gen_names.sh
@@ -10,6 +10,8 @@ cd /jbrowse/data
 AWS_ACCESS_KEY_ID=$AWSACCESS AWS_SECRET_ACCESS_KEY=$AWSSECRET aws s3 cp --recursive s3://agrjbrowse/MOD-jbrowses/SGD/jbrowse/data/seq seq/
 curl -O https://stage.dh18454tea1gu.amplifyapp.com/data/trackList.json
 curl -O https://stage.dh18454tea1gu.amplifyapp.com/data/tracks.conf
+
+# could limit this download to just the tracks needed for generate_names 
 AWS_ACCESS_KEY_ID=$AWSACCESS AWS_SECRET_ACCESS_KEY=$AWSSECRET aws s3 cp --recursive s3://agrjbrowse/MOD-jbrowses/SGD/jbrowse/data/tracks tracks/
 
 # convert absolut templateUrls to relataive
@@ -18,12 +20,20 @@ sed -i 's/https:.*SGD\/jbrowse\/data\///g' tracks.conf
 
 cd ..
 
+bin/flatfile-to-json.pl --gff /saccharomyces_cerevisiae.gff --trackLabel S288C --trackType CanvasFeatures
+bin/flatfile-to-json.pl --gff /saccharomyces_cerevisiae.gff --trackLabel "All Annotated Sequence Features" --trackType CanvasFeatures --type ARS,non_transcribed_region,blocked_reading_frame,centromere,chromosome,gene,long_terminal_repeat,LTR_retrotransposon,mating_type_region,ncRNA_gene,origin_of_replication,pseudogene,rRNA_gene,silent_mating_type_cassette_array,snoRNA_gene,snRNA_gene,telomerase_RNA_gene,telomere,transposable_element_gene,tRNA_gene
+bin/flatfile-to-json.pl --gff /saccharomyces_cerevisiae.gff --trackLabel Protein-Coding-Genes --trackType CanvasFeatures --type CDS,five_prime_UTR_intron,gene,intein_encoding_region,mRNA,plus_1_translational_frameshift,transposable_element_gene
+bin/flatfile-to-json.pl --gff /saccharomyces_cerevisiae.gff --trackLabel Non-Coding-RNA-Genes --trackType CanvasFeatures --type ncRNA_gene,rRNA_gene,snoRNA_gene,snRNA_gene,tRNA_gene
+bin/flatfile-to-json.pl --gff /saccharomyces_cerevisiae.gff --trackLabel Subfeatures --trackType CanvasFeatures --type ARS_consensus_sequence,CDS,centromere_DNA_Element_I,centromere_DNA_Element_II,centromere_DNA_Element_III,external_transcribed_spacer_region,five_prime_UTR_intron,intein_encoding_region,internal_transcribed_spacer_region,intron,noncoding_exon,non_transcribed_region,plus_1_translational_frameshift,telomeric_repeat,W_region,X_element,X_element_combinatorial_repeat,X_region,Y_prime_element,Y_region,Z1_region,Z2_region
+
+
+
 # simple index everything command
 #bin/generate-names.pl 
 
 
 #specifying what tracks to index
-bin/generate-names.pl -v --tracks \
+bin/generate-names.pl --tracks \
 "All Annotated Sequence Features",\
 "cDNA_transcripts",\
 "longest_full-ORF_transcripts_ypd",\
@@ -39,5 +49,6 @@ bin/generate-names.pl -v --tracks \
 
 echo "uploading to s3"
 
+AWS_ACCESS_KEY_ID=$AWSACCESS AWS_SECRET_ACCESS_KEY=$AWSSECRET aws s3 cp --quiet --recursive --acl public-read data/tracks s3://agrjbrowse/test/sgd_tracks/
 AWS_ACCESS_KEY_ID=$AWSACCESS AWS_SECRET_ACCESS_KEY=$AWSSECRET aws s3 cp --quiet --recursive --acl public-read data/names s3://agrjbrowse/test/sgd-names/
 
